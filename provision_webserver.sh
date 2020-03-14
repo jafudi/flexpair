e9559ce895c3bb0437d2e6c514f49724e5290e9c
healideal <mail@jens-fielenbach.de>
2020-03-14 13:57:24 +0100

    This is a necessary workaround in order to neutralize a bug in VirtualBox shared folders, according to the Vagrant docs: https://www.vagrantup.com/docs/synced-folders/virtualbox.html#caveats

--- provision_webserver.sh
+++ provision_webserver.sh
@@ -1,6 +1,13 @@
+#!/usr/bin/env bash
+
 apt-get update
 apt-get install -y apache2
 if ! [ -L /var/www ]; then
     rm -rf /var/www
     ln -fs /vagrant /var/www
 fi
+
+# This is a necessary workaround in order to neutralize a bug
+# in VirtualBox shared folders, according to the Vagrant docs
+# https://www.vagrantup.com/docs/synced-folders/virtualbox.html#caveats
+echo -e "EnableSendfile Off" >> /etc/apache2/apache2.conf
