71ae15ea66c9a7c8b40fe2cc340e252b6fa27408
healideal <mail@jens-fielenbach.de>
2020-03-17 21:39:06 +0100

    sudo docker pull

--- provision_docker.sh
+++ provision_docker.sh
@@ -1,6 +1,5 @@
 setxkbmap -layout 'ch(de)'
 
-echo "\n\nInstall Docker on guest OS..."
 sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -8,5 +7,4 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends docker-ce docker-ce-cli containerd.io
 
-echo "\n\nPull Python Docker image which all releases share..."
 sudo docker pull jafudi/idea-extractor:latest
