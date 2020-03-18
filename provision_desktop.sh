e64f76880e43cccc2863a7baf7edf2206e060c14
healideal <mail@jens-fielenbach.de>
2020-03-19 00:16:23 +0100

    sudo rm -f /etc/default/grub.d/40-force-partuuid.cfg

--- provision_desktop.sh
+++ provision_desktop.sh
@@ -5,6 +5,8 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends u
 setxkbmap -layout 'ch(de)'
 
 echo 'GRUB_DISABLE_LINUX_UUID=true' | sudo tee -a /etc/default/grub
+cat /etc/default/grub.d/40-force-partuuid.cfg
+sudo rm -f /etc/default/grub.d/40-force-partuuid.cfg
 sudo update-grub
 
 sudo reboot
