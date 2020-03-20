9a162aef8c85053efbc63eb3232cefc9456e3953
healideal <mail@jens-fielenbach.de>
2020-03-20 20:22:27 +0100

    Test provisioners separately.

--- provision_desktop.sh
+++ provision_desktop.sh
@@ -1,12 +1,10 @@
 sudo apt-get update
 
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends ubuntu-desktop
-
-setxkbmap -layout 'ch(de)'
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y ubuntu-desktop
 
 echo 'GRUB_DISABLE_LINUX_UUID=true' | sudo tee -a /etc/default/grub
-# https://help.ubuntu.com/community/Grub2/Setup#User_Settings:_.2Fetc.2Fdefault.2Fgrub
-sudo rm -f /etc/default/grub.d/40-force-partuuid.cfg
+## https://help.ubuntu.com/community/Grub2/Setup#User_Settings:_.2Fetc.2Fdefault.2Fgrub
+#sudo rm -f /etc/default/grub.d/40-force-partuuid.cfg
 sudo update-grub
 
 sudo reboot
