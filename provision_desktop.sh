aa2dafcd1af30cc1a08d6501fd107fad60bc2f85
healideal <mail@jens-fielenbach.de>
2020-03-20 20:57:44 +0100

    Try to cloud build Ubuntu only with Desktop installed.

--- provision_desktop.sh
+++ provision_desktop.sh
@@ -4,7 +4,7 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get install -y ubuntu-desktop
 
 echo 'GRUB_DISABLE_LINUX_UUID=true' | sudo tee -a /etc/default/grub
 ## https://help.ubuntu.com/community/Grub2/Setup#User_Settings:_.2Fetc.2Fdefault.2Fgrub
-#sudo rm -f /etc/default/grub.d/40-force-partuuid.cfg
+sudo rm -f /etc/default/grub.d/40-force-partuuid.cfg
 sudo update-grub
 
 sudo reboot
