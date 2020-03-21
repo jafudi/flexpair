cff27b33722f20cdffc98306476f56ee2bae550d
healideal <mail@jens-fielenbach.de>
2020-03-22 00:30:32 +0100

    Move out of Downloads somewhere else before booting. Dunno why.

--- provision_desktop.sh
+++ provision_desktop.sh
@@ -1,11 +1,16 @@
 sudo apt-get update
 
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends ubuntu-desktop
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y ubuntu-desktop
 
-# https://www.gnu.org/software/grub/manual/grub/html_node/Simple-configuration.html
-cat  /etc/default/grub
 echo 'GRUB_DISABLE_LINUX_UUID=true' | sudo tee -a /etc/default/grub
+echo 'GRUB_CMDLINE_LINUX_DEFAULT=""' | sudo tee -a /etc/default/grub
+echo '/etc/default/grub:'
+cat /etc/default/grub
+echo '/etc/default/grub.d/40-force-partuuid.cfg:'
+cat /etc/default/grub.d/40-force-partuuid.cfg
 sudo rm -f /etc/default/grub.d/40-force-partuuid.cfg
+echo '/etc/default/grub.d/50-cloudimg-settings.cfg:'
+cat /etc/default/grub.d/50-cloudimg-settings.cfg
 sudo rm -f /etc/default/grub.d/50-cloudimg-settings.cfg
 sudo update-grub
 
