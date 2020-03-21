65c2bfea7571576194097ba09e0453479cb9d5c5
healideal <mail@jens-fielenbach.de>
2020-03-21 18:49:27 +0100

    install recommends

--- provision_desktop.sh
+++ provision_desktop.sh
@@ -1,10 +1,9 @@
 sudo apt-get update
 
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends ubuntu-desktop
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y ubuntu-desktop
 
 # https://www.gnu.org/software/grub/manual/grub/html_node/Simple-configuration.html
 echo 'GRUB_DISABLE_LINUX_UUID=true' | sudo tee -a /etc/default/grub
-echo 'GRUB_CMDLINE_LINUX="scsi_mod.use_blk_mq=Y"' | sudo tee -a /etc/default/grub
 echo 'GRUB_CMDLINE_LINUX_DEFAULT=""' | sudo tee -a /etc/default/grub
 sudo rm -f /etc/default/grub.d/40-force-partuuid.cfg
 sudo rm -f /etc/default/grub.d/50-cloudimg-settings.cfg
