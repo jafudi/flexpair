9ae04a72762d5349c8368ab441dc48c6938cb7de
healideal <mail@jens-fielenbach.de>
2020-03-21 16:37:24 +0100

    echo 'GRUB_CMDLINE_LINUX="scsi_mod.use_blk_mq=Y"' | sudo tee -a /etc/default/grub

--- provision_desktop.sh
+++ provision_desktop.sh
@@ -4,7 +4,7 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends u
 
 # https://www.gnu.org/software/grub/manual/grub/html_node/Simple-configuration.html
 echo 'GRUB_DISABLE_LINUX_UUID=true' | sudo tee -a /etc/default/grub
-echo 'GRUB_CMDLINE_LINUX="scsi_mod.use_blk_mq=Y' | sudo tee -a /etc/default/grub
+echo 'GRUB_CMDLINE_LINUX="scsi_mod.use_blk_mq=Y"' | sudo tee -a /etc/default/grub
 echo 'GRUB_CMDLINE_LINUX_DEFAULT=""' | sudo tee -a /etc/default/grub
 sudo rm -f /etc/default/grub.d/40-force-partuuid.cfg
 sudo rm -f /etc/default/grub.d/50-cloudimg-settings.cfg
