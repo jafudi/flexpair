9f03ba84e6f1bc3ec4bd2ae12c5651cca52eaa6a
healideal <mail@jens-fielenbach.de>
2020-03-21 19:02:58 +0100

    minimal

--- provision_desktop.sh
+++ provision_desktop.sh
@@ -1,8 +1,9 @@
 sudo apt-get update
 
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y ubuntu-desktop
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends ubuntu-desktop
 
 # https://www.gnu.org/software/grub/manual/grub/html_node/Simple-configuration.html
+cat  /etc/default/grub
 echo 'GRUB_DISABLE_LINUX_UUID=true' | sudo tee -a /etc/default/grub
 echo 'GRUB_CMDLINE_LINUX_DEFAULT=""' | sudo tee -a /etc/default/grub
 sudo rm -f /etc/default/grub.d/40-force-partuuid.cfg
