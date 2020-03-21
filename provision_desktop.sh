73dfb46383793c440e3c05495a3381fd067f3c99
healideal <mail@jens-fielenbach.de>
2020-03-21 09:58:22 +0100

    Adapt GRUB config so that image becomes bootable locally.

--- provision_desktop.sh
+++ provision_desktop.sh
@@ -5,7 +5,13 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get install -y ubuntu-desktop
 ## https://help.ubuntu.com/community/Grub2/Setup#User_Settings:_.2Fetc.2Fdefault.2Fgrub
 echo 'GRUB_DISABLE_LINUX_UUID=true' | sudo tee -a /etc/default/grub
 echo 'GRUB_CMDLINE_LINUX_DEFAULT=""' | sudo tee -a /etc/default/grub
+echo '/etc/default/grub:'
+cat /etc/default/grub
+echo '/etc/default/grub.d/40-force-partuuid.cfg:'
+cat /etc/default/grub.d/40-force-partuuid.cfg
 sudo rm -f /etc/default/grub.d/40-force-partuuid.cfg
+echo '/etc/default/grub.d/50-cloudimg-settings.cfg:'
+cat /etc/default/grub.d/50-cloudimg-settings.cfg
 sudo rm -f /etc/default/grub.d/50-cloudimg-settings.cfg
 sudo update-grub
 
