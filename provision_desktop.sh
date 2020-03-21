d154b8abd7f4bf3a1dd74bc6224485e2714b2a5b
healideal <mail@jens-fielenbach.de>
2020-03-21 12:06:54 +0100

    Test whether GRUB_CMDLINE_LINUX="scsi_mod.use_blk_mq=Y' works or not

--- provision_desktop.sh
+++ provision_desktop.sh
@@ -2,16 +2,11 @@ sudo apt-get update
 
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y ubuntu-desktop
 
-## https://help.ubuntu.com/community/Grub2/Setup#User_Settings:_.2Fetc.2Fdefault.2Fgrub
-echo '/etc/default/grub:'
-cat /etc/default/grub
+# https://www.gnu.org/software/grub/manual/grub/html_node/Simple-configuration.html
 echo 'GRUB_DISABLE_LINUX_UUID=true' | sudo tee -a /etc/default/grub
 echo 'GRUB_CMDLINE_LINUX="scsi_mod.use_blk_mq=Y' | sudo tee -a /etc/default/grub
-echo '/etc/default/grub.d/40-force-partuuid.cfg:'
-cat /etc/default/grub.d/40-force-partuuid.cfg
+echo 'GRUB_CMDLINE_LINUX_DEFAULT=""' | sudo tee -a /etc/default/grub
 sudo rm -f /etc/default/grub.d/40-force-partuuid.cfg
-echo '/etc/default/grub.d/50-cloudimg-settings.cfg:'
-cat /etc/default/grub.d/50-cloudimg-settings.cfg
 sudo rm -f /etc/default/grub.d/50-cloudimg-settings.cfg
 sudo update-grub
 
