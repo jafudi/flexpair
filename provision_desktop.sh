9dcb3f5c4390362c0774d10463a35e52ad094365
healideal <mail@jens-fielenbach.de>
2020-03-21 21:03:53 +0100

    minimal

--- provision_desktop.sh
+++ provision_desktop.sh
@@ -5,7 +5,6 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends u
 # https://www.gnu.org/software/grub/manual/grub/html_node/Simple-configuration.html
 cat  /etc/default/grub
 echo 'GRUB_DISABLE_LINUX_UUID=true' | sudo tee -a /etc/default/grub
-echo 'GRUB_CMDLINE_LINUX_DEFAULT=""' | sudo tee -a /etc/default/grub
 sudo rm -f /etc/default/grub.d/40-force-partuuid.cfg
 sudo rm -f /etc/default/grub.d/50-cloudimg-settings.cfg
 sudo update-grub
