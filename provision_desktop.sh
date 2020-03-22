7459198d2be892a6c50100623f55cf2e965991d8
healideal <mail@jens-fielenbach.de>
2020-03-23 00:04:09 +0100

    Try this https://mymemorysucks.wordpress.com/2016/10/27/how-to-install-a-minimal-ubuntu-16-10-desktop/

--- provision_desktop.sh
+++ provision_desktop.sh
@@ -1,17 +1,23 @@
+export DEBIAN_FRONTEND=noninteractive
 sudo apt-get update
-
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends ubuntu-desktop
+sudo apt-get dist-upgrade
+sudo apt-get install -y lightdm
+sudo apt-get install -y unity
+sudo apt-get install -y --no-install-recommends ubuntu-desktop
 
 echo 'GRUB_DISABLE_LINUX_UUID=true' | sudo tee -a /etc/default/grub
 echo 'GRUB_CMDLINE_LINUX_DEFAULT=""' | sudo tee -a /etc/default/grub
-echo '/etc/default/grub:'
+echo 'Updated /etc/default/grub:'
 cat /etc/default/grub
+
 echo 'Delete /etc/default/grub.d/40-force-partuuid.cfg:'
 cat /etc/default/grub.d/40-force-partuuid.cfg
 sudo rm -f /etc/default/grub.d/40-force-partuuid.cfg
+
 echo 'Delete /etc/default/grub.d/50-cloudimg-settings.cfg:'
 cat /etc/default/grub.d/50-cloudimg-settings.cfg
 sudo rm -f /etc/default/grub.d/50-cloudimg-settings.cfg
+
 sudo update-grub
 
 sudo reboot
