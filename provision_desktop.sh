2a7fd76e24c198849d20571b6b2991990c83e915
healideal <mail@jens-fielenbach.de>
2020-03-22 13:56:09 +0100

    Remove startup script.

--- provision_desktop.sh
+++ provision_desktop.sh
@@ -6,11 +6,7 @@ echo 'GRUB_DISABLE_LINUX_UUID=true' | sudo tee -a /etc/default/grub
 echo 'GRUB_CMDLINE_LINUX_DEFAULT=""' | sudo tee -a /etc/default/grub
 echo '/etc/default/grub:'
 cat /etc/default/grub
-echo '/etc/default/grub.d/40-force-partuuid.cfg:'
-cat /etc/default/grub.d/40-force-partuuid.cfg
 sudo rm -f /etc/default/grub.d/40-force-partuuid.cfg
-echo '/etc/default/grub.d/50-cloudimg-settings.cfg:'
-cat /etc/default/grub.d/50-cloudimg-settings.cfg
 sudo rm -f /etc/default/grub.d/50-cloudimg-settings.cfg
 sudo update-grub
 
