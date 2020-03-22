884143da85d95629798e5f56f50472ee574a7e80
healideal <mail@jens-fielenbach.de>
2020-03-23 00:45:08 +0100

    Switch back to lubuntu-core.

--- provision_desktop.sh
+++ provision_desktop.sh
@@ -1,9 +1,7 @@
 export DEBIAN_FRONTEND=noninteractive
 sudo apt-get update
-sudo apt-get dist-upgrade
-sudo apt-get install -y lightdm
-sudo apt-get install -y unity
-sudo apt-get install -y --no-install-recommends ubuntu-desktop
+sudo apt-get upgrade
+sudo apt-get install -y lubuntu-core
 
 echo 'GRUB_DISABLE_LINUX_UUID=true' | sudo tee -a /etc/default/grub
 echo 'GRUB_CMDLINE_LINUX_DEFAULT=""' | sudo tee -a /etc/default/grub
