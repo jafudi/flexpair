91ac4b4273a54b141bf2e4b99a92f70bad19d973
healideal <mail@jens-fielenbach.de>
2020-03-23 11:40:34 +0100

    network-config=disabled

--- provision_desktop.sh
+++ provision_desktop.sh
@@ -3,6 +3,7 @@ sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y lubuntu-core
 
 echo 'GRUB_DISABLE_LINUX_UUID=true' | sudo tee -a /etc/default/grub
+# https://cloudinit.readthedocs.io/en/latest/topics/network-config.html#disabling-network-configuration
 echo 'GRUB_CMDLINE_LINUX_DEFAULT="network-config=disabled"' | sudo tee -a /etc/default/grub
 echo 'Updated /etc/default/grub:'
 cat /etc/default/grub
