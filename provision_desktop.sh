6d49a0af8eb65ad84a4b8bd479cea9ab8328ef53
healideal <mail@jens-fielenbach.de>
2020-03-23 13:23:29 +0100

    Boot into text (command line) mode first.

--- provision_desktop.sh
+++ provision_desktop.sh
@@ -1,19 +1,17 @@
+# Install a lightweight desktop
 sudo apt-get clean
 sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y lubuntu-core
 
-echo 'GRUB_DISABLE_LINUX_UUID=true' | sudo tee -a /etc/default/grub
-# https://cloudinit.readthedocs.io/en/latest/topics/network-config.html#disabling-network-configuration
-echo 'GRUB_CMDLINE_LINUX_DEFAULT="cloud-init=disabled network-config=disabled"' | sudo tee -a /etc/default/grub
-echo 'Updated /etc/default/grub:'
-cat /etc/default/grub
+# Disable cloud-init as described in its documentation
+echo 'GRUB_CMDLINE_LINUX_DEFAULT="text cloud-init=disabled network-config=disabled"' | sudo tee -a /etc/default/grub
+sudo touch /etc/cloud/cloud-init.disabled
 
-echo 'Delete /etc/default/grub.d/40-force-partuuid.cfg:'
-cat /etc/default/grub.d/40-force-partuuid.cfg
+# Disable reference to partitions via UUID
+echo 'GRUB_DISABLE_LINUX_UUID=true' | sudo tee -a /etc/default/grub
 sudo rm -f /etc/default/grub.d/40-force-partuuid.cfg
 
-echo 'Delete /etc/default/grub.d/50-cloudimg-settings.cfg:'
-cat /etc/default/grub.d/50-cloudimg-settings.cfg
+# Remove other overwrites by Google Cloud Platform
 sudo rm -f /etc/default/grub.d/50-cloudimg-settings.cfg
 
 sudo update-grub
