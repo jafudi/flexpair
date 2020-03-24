b9dfc4855ba30652674ec2972c13b93f9f684bc1
healideal <mail@jens-fielenbach.de>
2020-03-24 11:10:59 +0100

    sudo apt-get update

--- provision_docker.sh
+++ provision_docker.sh
@@ -1,5 +1,7 @@
 sudo apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends apt-transport-https ca-certificates curl gnupg-agent
+sudo apt-get update
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y software-properties-common
 sudo apt-get update
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
