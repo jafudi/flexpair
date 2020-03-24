1068c2266c67499f86c07e34058afb7ff6ba50df
healideal <mail@jens-fielenbach.de>
2020-03-25 00:00:35 +0100

    Purge Google cloud-init configuration and reinstall

--- provision_desktop.sh
+++ provision_desktop.sh
@@ -3,17 +3,23 @@ sudo apt-get clean
 sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends lubuntu-desktop
 
-# Configure GRUB boot manager
-echo 'GRUB_HIDDEN_TIMEOUT=10' | sudo tee -a /etc/default/grub
-echo 'GRUB_HIDDEN_TIMEOUT_QUIET=true' | sudo tee -a /etc/default/grub
+# Remove password requirement
+sudo passwd -d jafudi
+
+# Purge Google cloud-init configuration and reinstall
+sudo apt-get purge -y cloud-init
+sudo rm -rf /etc/cloud/; sudo rm -rf /var/lib/cloud/
+sudo apt-get install -y cloud-init
+
+# Configure GRUB boot manager for desktop use
 echo 'GRUB_CMDLINE_LINUX="ds=nocloud"' | sudo tee -a /etc/default/grub
 echo 'GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"' | sudo tee -a /etc/default/grub
+echo 'GRUB_HIDDEN_TIMEOUT=10' | sudo tee -a /etc/default/grub
+echo 'GRUB_HIDDEN_TIMEOUT_QUIET=true' | sudo tee -a /etc/default/grub
 echo 'GRUB_DISABLE_LINUX_UUID=true' | sudo tee -a /etc/default/grub
 sudo rm -f /etc/default/grub.d/40-force-partuuid.cfg
 sudo rm -f /etc/default/grub.d/50-cloudimg-settings.cfg
 sudo update-grub
 
-# Remove password requirement
-sudo passwd -d jafudi
 
-sudo reboot
+
