034c52d713da98dc8399342f866ca2e9ce7bd044
healideal <mail@jens-fielenbach.de>
2020-03-24 13:18:19 +0100

    try lubuntu-desktop instead of lubuntu-core

--- provision_desktop.sh
+++ provision_desktop.sh
@@ -1,7 +1,7 @@
 # Install a lightweight desktop
 sudo apt-get clean
 sudo apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y lubuntu-core
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y lubuntu-desktop
 
 # Disable cloud-init as described in its documentation
 echo 'GRUB_HIDDEN_TIMEOUT=10' | sudo tee -a /etc/default/grub
@@ -17,6 +17,6 @@ sudo rm -f /etc/default/grub.d/40-force-partuuid.cfg
 sudo rm -f /etc/default/grub.d/50-cloudimg-settings.cfg
 sudo rm -f /var/run/google.startup.script
 
-sudo usermod -a -G  nopasswdlogin jafudi
+sudo usermod -a -G nopasswdlogin jafudi
 
 sudo update-grub
