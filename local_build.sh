f671cdcc2a813d41dbefa23c5532c0f0196c0c61
healideal <mail@jens-fielenbach.de>
2020-03-29 00:05:41 +0100

    Try to build via googlecompute using the same provisioners like locally.

---
+++ local_build.sh
@@ -0,0 +1,2 @@
+cd packer-desktop/ubuntu
+packer build -only=virtualbox-iso pack-lubuntu.json
--- update_box.sh
+++
@@ -1,4 +0,0 @@
-cd packer-desktop/ubuntu
-packer build lubuntu-desktop-19.10-amd64.json
-cd ../../
-vagrant box add --force --name lubuntu19.10 builds/lubuntu-desktop-19.10.virtualbox.box
