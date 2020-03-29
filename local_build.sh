0bbf3594f69b51a2229645d1277332fa230808ac
healideal <mail@jens-fielenbach.de>
2020-03-29 03:15:07 +0200

    timeout: 2400s

--- local_build.sh
+++ local_build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # https://packer.io/docs/builders/virtualbox-iso.html
-packer build -only=virtualbox-iso packer-desktop/ubuntu/pack-lubuntu.json
+packer build -force -only=virtualbox-iso packer-desktop/ubuntu/pack-lubuntu.json
