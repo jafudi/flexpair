67bbbb4227b249b7683e89d126cd80c3ab6d3bca
healideal <mail@jens-fielenbach.de>
2020-04-09 22:50:26 +0200

    Change language to German.

--- packer-desktop/ubuntu/scripts/lubuntu-desktop.sh
+++ packer-desktop/ubuntu/scripts/lubuntu-desktop.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -eux
 
-DEBIAN_FRONTEND="noninteractive" apt-get install -y lubuntu-desktop gdm3-
+DEBIAN_FRONTEND="noninteractive" apt-get autoremove -y --purge ubuntu-desktop kubuntu-desktop xubuntu-desktop
+DEBIAN_FRONTEND="noninteractive" apt-get install -y --no-install-recommends lubuntu-desktop gdm3- featherpad
 
 reboot
