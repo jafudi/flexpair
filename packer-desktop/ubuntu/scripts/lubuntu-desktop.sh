62b1a6fb598288c62f33bc37a00f2836832ec041
healideal <mail@jens-fielenbach.de>
2020-04-10 00:27:02 +0200

    Need a restart after configuring sddm for autologin

--- packer-desktop/ubuntu/scripts/lubuntu-desktop.sh
+++ packer-desktop/ubuntu/scripts/lubuntu-desktop.sh
@@ -3,4 +3,16 @@
 DEBIAN_FRONTEND="noninteractive" apt-get autoremove -y --purge ubuntu-desktop kubuntu-desktop xubuntu-desktop
 DEBIAN_FRONTEND="noninteractive" apt-get install -y --no-install-recommends lubuntu-desktop gdm3- featherpad
 
+cat << EOF > /etc/sddm.conf
+[Autologin]
+# Whether sddm should automatically log back into sessions when they exit
+Relogin=false
+
+# Name of session file for autologin session (if empty try last logged in)
+Session=Lubuntu.desktop
+
+# Username for autologin session
+User=vagrant
+EOF
+
 reboot
