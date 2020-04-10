04ada8da2a392a119731b3eca60aee6e5ab529bd
healideal <mail@jens-fielenbach.de>
2020-04-10 22:05:38 +0200

    Mount host folder to /home/vagrant/host

--- provision_webserver.sh
+++ provision_webserver.sh
@@ -2,11 +2,13 @@ apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends apache2
 if ! [ -L /var/www ]; then
     rm -rf /var/www
-    ln -fs /vagrant /var/www
+    ln -fs /home/vagrant/host /var/www
 fi
 
 # This is a necessary workaround in order to neutralize a bug
 # in VirtualBox shared folders, according to the Vagrant docs
 # https://www.vagrantup.com/docs/synced-folders/virtualbox.html#caveats
 echo -e "EnableSendfile Off" >> /etc/apache2/apache2.conf
+
+sudo /etc/init.d/apache2 restart
 
