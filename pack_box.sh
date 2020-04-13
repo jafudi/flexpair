d66cddd5a32f46a834624ac8a5ed55113b874e2d
healideal <mail@jens-fielenbach.de>
2020-04-13 16:05:54 +0200

    Add created vagrant box locally for testing before upload to Vagrant Cloud

--- pack_box.sh
+++ pack_box.sh
@@ -2,3 +2,5 @@
 
 # https://packer.io/docs/builders/virtualbox-iso.html
 packer build -force -only=virtualbox-iso packer-desktop/ubuntu/pack-lubuntu.json
+vagrant box remove --all Jafudi/ludopy
+vagrant box add --clean --force Jafudi/ludopy packer-desktop/builds/lubuntu-docker-python.virtualbox.box
