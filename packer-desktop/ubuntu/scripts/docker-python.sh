a7f391c22d31251f747ef78898eb9976f6ea3944
healideal <mail@jens-fielenbach.de>
2020-04-24 23:54:30 +0200

    Upgrade to Ubuntu 20.04 LTS. Google Cloud Build will fail because of changed docker repository.

--- packer-desktop/ubuntu/scripts/docker-python.sh
+++ packer-desktop/ubuntu/scripts/docker-python.sh
@@ -1,13 +1,8 @@
 #!/bin/bash -eux
 
 apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends apt-transport-https ca-certificates curl gnupg-agent
-apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get install -y software-properties-common
-apt-get update
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends docker-ce docker-ce-cli containerd.io
+DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends docker.io
+systemctl enable --now docker
+usermod -aG docker vagrant
 
 docker pull python:3.7
