d746724d6989c1bf19c2eb60d0d5666088393917
healideal <mail@jens-fielenbach.de>
2020-05-07 16:38:51 +0200

    Name vm after commit hash

--- build_from_scratch.sh
+++ build_from_scratch.sh
@@ -12,19 +12,26 @@ vagrant up
 
 vagrant halt
 
-cp oci_config ~/Library/VirtualBox/oci_config # on Mac
+if [ -z "$(git status --porcelain)" ]; then
+    # Working directory clean
+    cp oci_config ~/Library/VirtualBox/oci_config # on Mac
 
-VBoxManage export lubuntu-docker-python \
---output OCI:// \
---cloud 0 \
---vmname staging-vm-2020-05-07 \
---cloudprofile JafudiOnOCI \
---cloudbucket bucket-20200425-0937 \
---cloudshape VM.Standard2.1 \
---clouddomain iEUO:EU-FRANKFURT-1-AD-3 \
---clouddisksize 64 \
---cloudocivcn ocid1.vcn.oc1.eu-frankfurt-1.amaaaaaasxbb7uqahhczda6xm5rwgph4lj2ojvz3wzxu2lzdpstkd2bogrda \
---cloudocisubnet ocid1.subnet.oc1.eu-frankfurt-1.aaaaaaaaym5b7erow7ygf5msauzxgqwrs3vn6hjdwg2ebgght7azfjb7tjtq \
---cloudkeepobject true \
---cloudlaunchinstance true \
---cloudpublicip true
+    VBoxManage export lubuntu-docker-python \
+    --output OCI:// \
+    --cloud 0 \
+    --vmname "commit-$(git rev-parse --verify HEAD)"\
+    --cloudprofile JafudiOnOCI \
+    --cloudbucket bucket-20200425-0937 \
+    --cloudshape VM.Standard2.1 \
+    --clouddomain iEUO:EU-FRANKFURT-1-AD-3 \
+    --clouddisksize 64 \
+    --cloudocivcn ocid1.vcn.oc1.eu-frankfurt-1.amaaaaaasxbb7uqahhczda6xm5rwgph4lj2ojvz3wzxu2lzdpstkd2bogrda \
+    --cloudocisubnet ocid1.subnet.oc1.eu-frankfurt-1.aaaaaaaaym5b7erow7ygf5msauzxgqwrs3vn6hjdwg2ebgght7azfjb7tjtq \
+    --cloudkeepobject true \
+    --cloudlaunchinstance true \
+    --cloudpublicip true
+else
+  # Uncommitted changes
+  echo "Commit any changes before proceeding."
+fi
+
