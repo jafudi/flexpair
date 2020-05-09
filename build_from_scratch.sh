9913145167364fa8d0c68c2a184a4bfcc713bf72
healideal <mail@jens-fielenbach.de>
2020-05-09 13:30:51 +0200

    Found out that cloud-init will only be triggered on machines exported via the GUI and set to paravirtualized mode.

--- build_from_scratch.sh
+++ build_from_scratch.sh
@@ -2,6 +2,8 @@
 
 vagrant destroy --force
 
+PACKER_LOG=1
+PACKER_LOG_PATH="packer-desktop/builds"
 # https://packer.io/docs/builders/virtualbox-iso.html
 packer build -force -only=virtualbox-iso packer-desktop/ubuntu/pack-lubuntu.json
 
@@ -10,31 +12,31 @@ vagrant box add --clean --force Jafudi/ludopy packer-desktop/builds/lubuntu-dock
 
 vagrant up
 
-vagrant halt
-
-if [ -z "$(git status --porcelain)" ]; then
-    # Working directory clean
-    cp oci_config ~/Library/VirtualBox/oci_config # on Mac
-
-    vm_name="traction-commit-$(git rev-parse --verify HEAD)"
-    echo "Assign machine name ${vm_name}..."
-
-    VBoxManage export lubuntu-docker-python \
-    --output OCI:// \
-    --cloud 0 \
-    --vmname ${vm_name} \
-    --cloudprofile JafudiOnOCI \
-    --cloudbucket bucket-20200425-0937 \
-    --cloudshape VM.Standard2.1 \
-    --clouddomain iEUO:EU-FRANKFURT-1-AD-3 \
-    --clouddisksize 64 \
-    --cloudocivcn ocid1.vcn.oc1.eu-frankfurt-1.amaaaaaasxbb7uqahhczda6xm5rwgph4lj2ojvz3wzxu2lzdpstkd2bogrda \
-    --cloudocisubnet ocid1.subnet.oc1.eu-frankfurt-1.aaaaaaaaym5b7erow7ygf5msauzxgqwrs3vn6hjdwg2ebgght7azfjb7tjtq \
-    --cloudkeepobject true \
-    --cloudlaunchinstance true \
-    --cloudpublicip true
-else
-  # Uncommitted changes
-  echo "Commit any changes before proceeding."
-fi
+#vagrant halt
+#
+#if [ -z "$(git status --porcelain)" ]; then
+#    # Working directory clean
+#    cp oci_config ~/Library/VirtualBox/oci_config # on Mac
+#
+#    vm_name="traction-commit-$(git rev-parse --verify HEAD)"
+#    echo "Assign machine name ${vm_name}..."
+#
+#    VBoxManage export lubuntu-docker-python \
+#    --output OCI:// \
+#    --cloud 0 \
+#    --vmname ${vm_name} \
+#    --cloudprofile JafudiOnOCI \
+#    --cloudbucket bucket-20200425-0937 \
+#    --cloudshape VM.Standard2.1 \
+#    --clouddomain iEUO:EU-FRANKFURT-1-AD-3 \
+#    --clouddisksize 64 \
+#    --cloudocivcn ocid1.vcn.oc1.eu-frankfurt-1.amaaaaaasxbb7uqahhczda6xm5rwgph4lj2ojvz3wzxu2lzdpstkd2bogrda \
+#    --cloudocisubnet ocid1.subnet.oc1.eu-frankfurt-1.aaaaaaaaym5b7erow7ygf5msauzxgqwrs3vn6hjdwg2ebgght7azfjb7tjtq \
+#    --cloudkeepobject true \
+#    --cloudlaunchinstance true \
+#    --cloudpublicip true
+#else
+#  # Uncommitted changes
+#  echo "Commit any changes before proceeding."
+#fi
 
