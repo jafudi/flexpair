ec6d35e4f23c9dc851d9a467eddf9c86e96a8626
healideal <mail@jens-fielenbach.de>
2020-05-09 23:22:04 +0200

    Probably need to become root

--- cloud-init/scripts/per-instance/200_customize_desktop.sh
+++ cloud-init/scripts/per-instance/200_customize_desktop.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 # https://manpages.ubuntu.com/manpages/focal/en/man1/pcmanfm-qt.1.html
-pcmanfm-qt --desktop-off
-pcmanfm-qt --desktop --daemon --set-wallpaper="/usr/share/lubuntu/wallpapers/2004-lubuntu-logo.png" --wallpaper-mode=stretch
+sudo pcmanfm-qt --desktop-off
+sudo pcmanfm-qt --desktop --daemon --set-wallpaper="/usr/share/lubuntu/wallpapers/2004-lubuntu-logo.png" --wallpaper-mode=stretch
