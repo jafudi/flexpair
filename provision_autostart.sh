e95d34dcd8f099ecb0151455fde0da3e95d3511d
healideal <mail@jens-fielenbach.de>
2020-05-11 19:02:19 +0200

    Bugfix

--- provision_autostart.sh
+++ provision_autostart.sh
@@ -2,6 +2,6 @@
 
 mkdir -p /home/vagrant/.config/autostart
 
-cp -fRv /var/tmp/traction/autostart/ /home/vagrant/.config/autostart
+cp /var/tmp/traction/autostart/* /home/vagrant/.config/autostart/
 
 find /home/vagrant/.config/autostart -type f -exec chmod +x {} \;
--- provision_x11vnc.sh
+++ provision_x11vnc.sh
@@ -3,6 +3,7 @@
 DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get install --upgrade -y --no-install-recommends x11vnc net-tools
+mkdir -p /home/vagrant/.vnc
 x11vnc -storepasswd "vagrant" /home/vagrant/.vnc/passwd
 
 
