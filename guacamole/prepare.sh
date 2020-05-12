fbd1ca880c773a13c7ea173ad0dc6f38e4258879
healideal <mail@jens-fielenbach.de>
2020-05-12 23:51:24 +0200

    vagrant needs to sudo in order to aquire root rights

--- guacamole/prepare.sh
+++ guacamole/prepare.sh
@@ -3,7 +3,7 @@
 echo "Preparing folder init and creating ./init/initdb.sql"
 mkdir ./init >/dev/null 2>&1
 mkdir -p ./nginx/ssl >/dev/null 2>&1
-chmod -R +x ./init
+sudo chmod -R +x ./init
 docker run --rm guacamole/guacamole /opt/guacamole/bin/initdb.sh --postgres > ./init/initdb.sql
 echo "done"
 echo "Creating SSL certificates"
