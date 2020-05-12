98ab6c2797447a01a25bf00ecaa6e0232c16716b
healideal <mail@jens-fielenbach.de>
2020-05-12 12:04:31 +0200

    Comment in

--- cloud-init/scripts/per-instance/200_serve_guacamole.sh
+++ cloud-init/scripts/per-instance/200_serve_guacamole.sh
@@ -2,11 +2,7 @@
 
 # when guacamole works add -localhost flag to x1vnc again for more security
 
-
-#curl --silent -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-#chmod +x /usr/local/bin/docker-compose
-#
-#git clone "https://github.com/boschkundendienst/guacamole-docker-compose.git"
-#cd guacamole-docker-compose
-#./prepare.sh
-#docker-compose up -d
+git clone "https://github.com/boschkundendienst/guacamole-docker-compose.git"
+cd guacamole-docker-compose
+./prepare.sh
+docker-compose up -d
