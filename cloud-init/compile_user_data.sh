7a823b675e8aae0d9b213bad9c7b27bb73d433f1
healideal <mail@jens-fielenbach.de>
2020-05-18 14:34:03 +0200

    Prepare cloud-init for nocloud-net mode

--- cloud-init/compile_user_data.sh
+++ cloud-init/compile_user_data.sh
@@ -2,7 +2,6 @@
 
 python3 tools/make-mime.py \
 -a user-data-parts/cloud-config.yaml:cloud-config \
--a user-data-parts/update_release.sh:x-shellscript \
 -a user-data-parts/include-once:x-include-once-url \
 -a user-data-parts/always-include:x-include-url \
 > seed-from/user-data
--- provision_cloud_init.sh
+++ provision_cloud_init.sh
@@ -7,5 +7,4 @@ apt-get install --upgrade -y --no-install-recommends cloud-init
 cp /var/tmp/traction/grub.cfg /etc/default/grub
 update-grub
 
-find /var/tmp/traction/cloud-init/scripts -type f -iname "*.sh" -exec chmod +x {} \;
 
