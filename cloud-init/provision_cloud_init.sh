584168a3725d74ebe863f2a3723d1f5202657bd7
healideal <mail@jens-fielenbach.de>
2020-05-18 23:49:44 +0200

    Disable modules to get a better overview

--- cloud-init/provision_cloud_init.sh
+++ cloud-init/provision_cloud_init.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+# Prepare for cloud-init to run at next boot
 DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get install --upgrade -y --no-install-recommends cloud-init
 cp /var/tmp/traction/cloud-init/cloud.cfg /etc/cloud/cloud.cfg
 
-cp /var/tmp/traction/grub.cfg /etc/default/grub
+# Prepare for Oracle Cloud Infrastructure
+cp /var/tmp/traction/cloud-init/grub.cfg /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 
 
