3b7f379d040ca82dd2d7663a6b27ad96b5561396
healideal <mail@jens-fielenbach.de>
2020-05-19 10:18:28 +0200

    Add all scripts to userdata

--- cloud-init/compile_user_data.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-python3 tools/make-mime.py \
--a user-data-parts/cloud-config.yaml:cloud-config \
--a user-data-parts/include-once:x-include-once-url \
--a user-data-parts/always-include:x-include-url \
-> seed-from/user-data
--- guacamole/serve_guacamole.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# when guacamole works add -localhost flag to x1vnc again for more security
-
-export GUACAMOLE_HOME=/var/tmp/traction/guacamole
-cd ${GUACAMOLE_HOME}
-./prepare.sh
-export ETH0_IP=$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
-docker-compose up -d
