7bd0528172d6c1fb0ee580f7c0b10cc752ff7231
healideal <mail@jens-fielenbach.de>
2020-05-20 21:41:04 +0200

    Disable Oracle as datasource. Try using LetsEncrypt.

--- cloud-init/provision_cloud_init.sh
+++ cloud-init/provision_cloud_init.sh
@@ -6,8 +6,9 @@ apt-get update
 apt-get install --upgrade -y --no-install-recommends cloud-init
 cp /var/tmp/traction/cloud-init/cloud.cfg /etc/cloud/cloud.cfg
 
+# How to merge with Oracle datasource?
+echo "datasource_list: [ NoCloud, None ]" | sudo tee /etc/cloud/cloud.cfg.d/90_dpkg.cfg
+
 # Prepare for Oracle Cloud Infrastructure
 cp /var/tmp/traction/cloud-init/grub.cfg /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
-
-
