87d8d6491cef2e2dcc61ada9c5584e565474936b
healideal <mail@jens-fielenbach.de>
2020-05-21 00:39:07 +0200

    Bugfix

--- guacamole/init-letsencrypt.sh
+++ guacamole/init-letsencrypt.sh
@@ -10,6 +10,7 @@ rsa_key_size=4096
 data_path="./certbot"
 email="socialnets@jafudi.com" # Adding a valid address is strongly recommended
 staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+export ETH0_IP=$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
 
 #if [ -d "$data_path" ]; then
 #  read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
