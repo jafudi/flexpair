72e00809a59efa0d4fffbf1448e56a0cae45d260
healideal <mail@jens-fielenbach.de>
2020-05-21 16:22:47 +0200

    Fix obvious bug in foreign script

--- guacamole/init-letsencrypt.sh
+++ guacamole/init-letsencrypt.sh
@@ -1,19 +1,11 @@
 #!/usr/bin/env bash
 
-domains=(mydesk.jafudi.net)
+domain="mydesk.jafudi.net"
 rsa_key_size=4096
 data_path="./letsencrypt/certbot"
 email="socialnets@jafudi.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 export ETH0_IP=$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
-
-#if [ -d "$data_path" ]; then
-#  read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
-#  if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
-#    exit
-#  fi
-#fi
-
 
 if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
   echo "### Downloading recommended TLS parameters ..."
@@ -23,9 +15,9 @@ if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/
   echo
 fi
 
-echo "### Creating dummy certificate for $domains ..."
-path="/etc/letsencrypt/live/$domains"
-mkdir -p "$data_path/conf/live/$domains"
+echo "### Creating dummy certificate for $domain ..."
+path="/etc/letsencrypt/live/$domain"
+mkdir -p "$data_path/conf/live/$domain"
 docker-compose run --rm --entrypoint "\
   openssl req -x509 -nodes -newkey rsa:1024 -days 1\
     -keyout '$path/privkey.pem' \
@@ -38,20 +30,16 @@ echo "### Starting nginx ..."
 docker-compose up --force-recreate -d nginx
 echo
 
-echo "### Deleting dummy certificate for $domains ..."
+echo "### Deleting dummy certificate for $domain ..."
 docker-compose run --rm --entrypoint "\
-  rm -Rf /etc/letsencrypt/live/$domains && \
-  rm -Rf /etc/letsencrypt/archive/$domains && \
-  rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
+  rm -Rf /etc/letsencrypt/live/$domain && \
+  rm -Rf /etc/letsencrypt/archive/$domain && \
+  rm -Rf /etc/letsencrypt/renewal/$domain.conf" certbot
 echo
 
 
-echo "### Requesting Let's Encrypt certificate for $domains ..."
-#Join $domains to -d args
-domain_args=""
-for domain in "${domains[@]}"; do
-  domain_args="$domain_args -d $domain"
-done
+echo "### Requesting Let's Encrypt certificate for $domain ..."
+domain_args="-d $domain"
 
 # Select appropriate email arg
 case "$email" in
