f93218615840ba9d96d4d353d97691eeb549700e
healideal <mail@jens-fielenbach.de>
2020-06-17 13:24:03 +0200

    Pseudo changes to trigger pipeline

--- cron/set_wallpaper.sh
+++ cron/set_wallpaper.sh
@@ -4,4 +4,6 @@ curl http://www.berghalde.com/picam/capt0000.jpg -o berghalde-heidelberg.jpg
 # TODO: Crop originating from left bottom corner, use ImageMagick
 pcmanfm-qt --desktop-off
 rm -f /home/ubuntu/.cache/pcmanfm-qt/default/* # necessary to trigger update of desktop
-pcmanfm-qt --desktop --set-wallpaper=berghalde-heidelberg.jpg --wallpaper-mode=stretch
+nohup pcmanfm-qt --desktop --set-wallpaper=berghalde-heidelberg.jpg --wallpaper-mode=stretch &
+echo
+
