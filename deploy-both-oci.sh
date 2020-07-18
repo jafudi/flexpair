bda7f94e18fd70e9e006caf17a2f643b9c04a839
healideal <mail@jens-fielenbach.de>
2020-07-18 11:04:56 +0200

    Had to reload nginx manually, but apart from that SSL works now.

--- deploy-both-oci.sh
+++ deploy-both-oci.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 packer build \
-        -var "ssl_sub_domain=fielenbach.freeddns.org" \
+        -var "ssl_sub_domain=fielenbach.theworkpc.com" \
         -on-error=abort \
         packer-desktop/oracle-cloud-free-setup.json
