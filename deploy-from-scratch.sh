f0badfcc3159a60af544558e64e8db1b34d1b030
healideal <mail@jens-fielenbach.de>
2020-07-29 18:06:22 +0200

    Exit if ssh-keygen fails

--- deploy-from-scratch.sh
+++ deploy-from-scratch.sh
@@ -2,16 +2,15 @@
 
 SSH_KEY_FOLDER="/Users/jens/PycharmProjects/traction/packer-desktop/uploads/ssh"
 mkdir -p ${SSH_KEY_FOLDER}
-PRIVKEY_FILE="${SSH_KEY_FOLDER}/vm_key
-PASSPHRASE=""
+PRIVKEY_FILE="${SSH_KEY_FOLDER}/vm_key"
 COMMENT="$USER@$HOSTNAME"
-ssh-keygen -t ed25519 -f ${PRIVKEY_FILE} -q -N ${PASSPHRASE} -C ${COMMENT}# <<< y
+ssh-keygen -t ed25519 -f ${PRIVKEY_FILE} -q -N "" -C ${COMMENT} || exit 1
 PUBKEY_FILE="${PRIVKEY_FILE}.pub"
 
 packer build \
 -var "ssl_sub_domain=tryno4.theworkpc.com" \
 -var "ssh_public_key=$(cat ${PUBKEY_FILE})" \
--var "private_key_file=$PRIVKEY_FILE" \
+-var "private_key_file=${PRIVKEY_FILE}" \
 -var "ssh_keypair_name=${COMMENT}" \
 -on-error=abort \
 packer-desktop/oracle-cloud-free-setup.json
