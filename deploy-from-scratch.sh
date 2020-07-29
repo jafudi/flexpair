53b4750ce69b88ac7de6abbe9faf70788cae9e92
healideal <mail@jens-fielenbach.de>
2020-07-29 17:39:38 +0200

    Switch from RSA to ed25519

--- deploy-from-scratch.sh
+++ deploy-from-scratch.sh
@@ -2,14 +2,16 @@
 
 SSH_KEY_FOLDER="/Users/jens/PycharmProjects/traction/packer-desktop/uploads/ssh"
 mkdir -p ${SSH_KEY_FOLDER}
-PRIVKEY_FILE="${SSH_KEY_FOLDER}/vm_key"
-ssh-keygen -b 2048 -t rsa -f ${PRIVKEY_FILE} -q -N "" # <<< y
+PRIVKEY_FILE="${SSH_KEY_FOLDER}/vm_key
+PASSPHRASE=""
+COMMENT="$USER@$HOSTNAME"
+ssh-keygen -t ed25519 -f ${PRIVKEY_FILE} -q -N ${PASSPHRASE} -C ${COMMENT}# <<< y
 PUBKEY_FILE="${PRIVKEY_FILE}.pub"
 
 packer build \
 -var "ssl_sub_domain=tryno3.theworkpc.com" \
 -var "ssh_public_key=$(cat ${PUBKEY_FILE})" \
 -var "private_key_file=$PRIVKEY_FILE" \
--var "ssh_keypair_name=$USER@$HOSTNAME" \
+-var "ssh_keypair_name=${COMMENT}" \
 -on-error=abort \
 packer-desktop/oracle-cloud-free-setup.json
