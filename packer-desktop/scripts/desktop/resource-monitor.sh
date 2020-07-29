0747a2386b2aae658ec0eb90a1396296ed70723f
healideal <mail@jens-fielenbach.de>
2020-07-29 17:43:08 +0200

    Add etherape

--- packer-desktop/scripts/desktop/resource-monitor.sh
+++ packer-desktop/scripts/desktop/resource-monitor.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-DEBIAN_FRONTEND="noninteractive" apt-get install -y --no-install-recommends --upgrade conky-all
+DEBIAN_FRONTEND="noninteractive" apt-get install -y --no-install-recommends --upgrade \
+conky-all \
+etherape
 
 # https://wiki.ubuntuusers.de/Conky/
 
