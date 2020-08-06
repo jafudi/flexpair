133a35e0fc6b440f97fe4ca2dd577c11755618b4
healideal <mail@jens-fielenbach.de>
2020-08-07 01:28:34 +0200

    Domain name applied via cloud-init not via packer, i.e. not baked into image

--- packer-desktop/scripts/desktop/mumble-pulseaudio.sh
+++ packer-desktop/scripts/desktop/mumble-pulseaudio.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-DEBIAN_FRONTEND="noninteractive" apt-get install -y --no-install-recommends --upgrade mumble
-
 # https://www.mumble.info
-# https://wiki.ubuntuusers.de/Mumble/
+DEBIAN_FRONTEND="noninteractive" apt-get install -y --no-install-recommends --upgrade mumble
 
 mkdir -p $HOME/.config/pulse
 cat << EOF > $HOME/.config/pulse/default.pa
@@ -23,42 +21,5 @@ load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1
 EOF
 chown ubuntu -R $HOME/.config/pulse
 
-mkdir -p $HOME/.config/Mumble
-cat << EOF > $HOME/.config/Mumble/Mumble.conf
-[audio]
-echomulti=false
-headphone=true
-input=PulseAudio
-noisesupress=0
-output=PulseAudio
-outputdelay=10
-quality=96000
-vadmax=@Variant(\0\0\0\x87\x38\0\x1\0)
-vadmin=@Variant(\0\0\0\x87\x38\0\x1\0)
-voicehold=250
 
-[codec]
-opus/encoder/music=true
-
-[net]
-autoconnect=true
-framesperpacket=6
-jitterbuffer=5
-
-[pulseaudio]
-output=MumbleNullSink
-
-[ui]
-developermenu=true
-WindowLayout=2
-server=${SSL_SUB_DOMAIN}
-username=Jafudi
-showcontextmenuinmenubar=true
-themestyle=Dark
-stateintray=false
-disablepubliclist=true
-disableconnectdialogediting=false
-EOF
-# https://wiki.natenom.de/mumble/benutzerhandbuch/mumble/variablen_mumble.ini
-chown ubuntu -R $HOME/.config/Mumble
 
