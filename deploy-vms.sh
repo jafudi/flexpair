0e1d73403c3b9816610191f78bea84e2a8f07bc7
healideal <mail@jens-fielenbach.de>
2020-08-06 17:09:01 +0200

    Automatically register DDNS records

--- deploy-vms.sh
+++ deploy-vms.sh
@@ -9,11 +9,16 @@ packer validate ${TARGET}  || exit 1
 
 FROM_SCRATCH=true
 EXCLUDE=""
+DYNU_API_KEY="56ge3636efbe35323352VX6d6c4dY4f5"
 
 # Loop through arguments and process them
 for arg in "$@"
 do
     case $arg in
+        -key=*|--api-key=*)
+        DYNU_API_KEY="${arg#*=}"
+        shift # Remove --api-key= from processing
+        ;;
         -desktop|--desktop-only)
         EXCLUDE="-except=guacamole-gateway"
         FROM_SCRATCH=false
@@ -44,7 +49,6 @@ case ${PING_STATUS} in
     ;;
     *)
     echo "Error: No DNS records seem to exist for this (sub)domain."
-    exit 1
     ;;
 esac
 
@@ -62,11 +66,19 @@ if ${FROM_SCRATCH} ; then
         sleep 5
         ;;
         2)
-        echo "Everything as expected :-)"
+        echo "The VM was probably already terminated."
         ;;
         *)
-        echo "Please register it with e.g. dynu.com."
-        exit 1
+        echo "Creating dynamic DNS record $EXCLUDE for domain ${SSL_DOMAIN}..."
+        DYNU_API_URL="https://api.dynu.com/v2"
+        curl -X POST "${DYNU_API_URL}/dns" \
+             -H "accept: application/json" \
+             -H "API-Key: ${DYNU_API_KEY}" \
+             -H "Content-Type: application/json" \
+             -d "{\"name\":\"${SSL_DOMAIN}\",\"ipv4Address\":\"1.2.3.4\",\"ttl\":120,\"ipv4\":true,\"ipv6\":false,\"ipv4WildcardAlias\":true,\"ipv6WildcardAlias\":false,\"allowZoneTransfer\":false,\"dnssec\":false}"
+        DYNU_STATUS=$(curl --silent -X GET "${DYNU_API_URL}/dns" \
+             -H "accept: application/json" \
+             -H "API-Key: ${DYNU_API_KEY}")
         ;;
     esac
     mkdir -p ${SSH_KEY_FOLDER}
@@ -82,13 +94,13 @@ else
         exit 1
         ;;
         *)
-        echo "Please register it with e.g. dynu.com."
+        echo "Please deploy from scratch."
         exit 1
         ;;
     esac
 fi
 
-echo "Provisioning VM(s) $EXCLUDE for domain ${SSL_DOMAIN}..."
+echo "Provisioning VM(s) $EXCLUDE"
 echo "This will take some minutes..."
 
 packer build \
