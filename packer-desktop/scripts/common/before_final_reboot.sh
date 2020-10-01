069b9ddb1d2c7b110d722896e9dc15f1e7a9c8a8
healideal <mail@jens-fielenbach.de>
2020-10-01 19:36:30 +0200

    Cloud providers which offer passing metadata/userdata will have baked in cloud-init already for sure

--- packer-desktop/scripts/common/before_final_reboot.sh
+++ packer-desktop/scripts/common/before_final_reboot.sh
@@ -1,19 +1,5 @@
 #!/usr/bin/env bash
 
-# https://cloudinit.readthedocs.io/en/latest/
-# https://help.ubuntu.com/community/CloudInit
-
-REQUIRED_PKG="cloud-init"
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
-echo Checking for $REQUIRED_PKG: $PKG_OK
-if [ "" = "$PKG_OK" ]; then
-    echo "Install cloud-init..."
-    # Prepare for cloud-init to run at next boot
-    DEBIAN_FRONTEND=noninteractive
-    apt-get update
-    apt-get install --upgrade -y --no-install-recommends cloud-init
-fi
-
 cloud-init clean --logs
 
 # https://github.com/letsdebug/letsdebug#problems-detected
