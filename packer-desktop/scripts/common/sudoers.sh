2f97c7396d64f08d850d672247652ace3ffd509f
healideal <mail@jens-fielenbach.de>
2020-10-03 01:21:08 +0200

    sudo chown -R ubuntu /home/ubuntu

--- packer-desktop/scripts/common/sudoers.sh
+++ packer-desktop/scripts/common/sudoers.sh
@@ -8,3 +8,5 @@ sudo sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=sudo' /etc/sudoe
 # Set up password-less sudo for the ubuntu user
 echo 'ubuntu ALL=(ALL) NOPASSWD:ALL' | sudo tee /etc/sudoers.d/99_user;
 sudo chmod 440 /etc/sudoers.d/99_user;
+
+sudo chown -R ubuntu /home/ubuntu
