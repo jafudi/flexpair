f097bf5d90d9d1c3473327298622614759350a07
healideal <mail@jens-fielenbach.de>
2020-10-07 09:35:35 +0200

    Create new TSIG key

--- internet-connection.tf
+++ internet-connection.tf
@@ -32,9 +32,9 @@ locals {
 provider "dns" {
   update {
     server        = "ns1.dynv6.com"
-    key_name      = "tsig-163844.dynv6.com."
+    key_name      = "tsig-163949.dynv6.com."
     key_algorithm = "hmac-sha512"
-    key_secret    = "f8W4Rp5+VDVXMfdsPTP7qcNki/HDf/IDwbR8hUQ+wTJArUD89bvVBO2vGgPEksdFTjKzT7hSkJbjkfDF28EO5g=="
+    key_secret    = "YVzr92fPuDkECv1fbrFc66aeubfp8fZH+94VbHMF0e7qGHy2t4KNAEMJ+vA5dW+dIVXfMJCjSltx9f7qVuu9HQ=="
   }
 }
 
