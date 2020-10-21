a00a228d26f5ae4693fc37ed786dacda77558c72
healideal <mail@jens-fielenbach.de>
2020-10-21 23:03:47 +0200

    Double square brackets

--- behave-tests.tf
+++ behave-tests.tf
@@ -15,6 +15,6 @@ resource "null_resource" "health_check" {
       URL  = "https://${local.subdomain}.${var.registered_domain}"
       CMD  = "curl --write-out '%%{http_code}' --silent --head --output /dev/null"
     }
-    command = "sleep $WAIT; RET=$($CMD $URL); [ $RET -eq 200 ] || exit 1;"
+    command = "sleep $WAIT; RET=$($CMD $URL); [[ $RET -eq 200 ]] || exit 1;"
   }
 }
