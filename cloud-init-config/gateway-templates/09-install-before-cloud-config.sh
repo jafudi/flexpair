43def04f236955bc85a933e660798cec621aba6c
healideal <mail@jens-fielenbach.de>
2020-10-25 23:54:11 +0100

    Bugfix

--- cloud-init-config/gateway-templates/09-install-before-cloud-config.sh
+++ cloud-init-config/gateway-templates/09-install-before-cloud-config.sh
@@ -8,6 +8,9 @@ sudo apt-get -qq purge unattended-upgrades snapd apport;
 sudo apt-get -qq autoremove
 
 # Update the package list
-sudo add-apt-repository universe
 sudo apt-get -qq update;
-sudo -E apt-get -qq install software-properties-common locales
+sudo -E apt-get -qq install software-properties-common
+sudo apt-get -qq update;
+sudo add-apt-repository universe
+sudo apt-get -qq update
+sudo -E apt-get -qq install locales
