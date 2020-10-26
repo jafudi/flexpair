30d69f65e40be25e4a902444a7513041a2f2abf1
healideal <mail@jens-fielenbach.de>
2020-10-26 22:46:55 +0100

    Move Guacamole init script to cloud-config.yaml for execution only once

--- cloud-init-config/gateway-templates/20-after-restart.sh
+++ cloud-init-config/gateway-templates/20-after-restart.sh
@@ -7,21 +7,6 @@ fi
 
 cd ${DOCKER_COMPOSE_FOLDER}
 
-echo "Preparing folder init and creating ./init/initdb.sql..."
-mkdir ./init >/dev/null 2>&1
-mkdir -p ./nginx/ssl >/dev/null 2>&1
-rm -rf ./data
-docker run --rm guacamole/guacamole /opt/guacamole/bin/initdb.sh --postgres > ./init/initdb.sql
-if [[ -s ./init/initdb.sql ]]; then
-    sudo chmod -R +x ./init
-    echo "done"
-else
-    echo "./init/initdb.sql was rendered empty. Exit."
-    exit 1
-fi
-
-echo "Bringing up Docker Compose..."
-echo
 docker-compose up
 
 cloud-init collect-logs
