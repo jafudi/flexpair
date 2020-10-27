86b338f484b7e32c951536013921a4d54bb64f33
healideal <mail@jens-fielenbach.de>
2020-10-27 09:46:08 +0100

    Bugfix typo

--- cloud-init-config/gateway-templates/99-after-restart.sh
+++ cloud-init-config/gateway-templates/99-after-restart.sh
@@ -11,17 +11,15 @@ usermod -aG docker ubuntu
 cd ${DOCKER_COMPOSE_FOLDER}
 
 curl  --silent -L "${DOCKER_COMPOSE_REPO}/docker-compose-$(uname -s)-$(uname -m)" \
-      -o /usr/local/bin/docker-compose ] && chmod +x /usr/local/bin/docker-compose
+      -o /usr/local/bin/docker-compose && chmod +x /usr/local/bin/docker-compose
 
 mkdir ./init >/dev/null 2>&1
 docker run --rm guacamole/guacamole /opt/guacamole/bin/initdb.sh --postgres > ./init/initdb.sql
 chmod -R +x ./init
 passwd -d ubuntu # for direct SSH access from guacd_container
 
-docker-compose up
-
 cloud-init collect-logs
 tar -xzf cloud-init.tar.gz
 rm -f cloud-init.tar.gz
-cd cloud-init-logs*
-cat cloud-init-output.log
+
+docker-compose up
