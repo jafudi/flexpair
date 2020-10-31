f80c3f40910161ace7319f22b54f013ebbfb2417
healideal <mail@jens-fielenbach.de>
2020-10-31 02:01:46 +0100

    Debugging

---
+++ cloud-init-config/gateway-templates/11-sudoers.sh
@@ -0,0 +1,7 @@
+#!/bin/sh -eux
+
+# Set up password-less sudo for the ubuntu user
+echo 'ubuntu ALL=(ALL) NOPASSWD:ALL' > /etc/sudoers.d/99_user;
+chmod 440 /etc/sudoers.d/99_user;
+
+chown -R ubuntu /home/ubuntu
