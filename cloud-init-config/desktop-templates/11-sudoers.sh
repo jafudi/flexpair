e10623adcb829a8c03028fa6b726b63fc0e16c92
healideal <mail@jens-fielenbach.de>
2020-11-01 11:22:56 +0100

    Should be relevant for Vagrant only

--- cloud-init-config/desktop-templates/11-sudoers.sh
+++ cloud-init-config/desktop-templates/11-sudoers.sh
@@ -3,8 +3,6 @@
 echo "Running script sudoers.sh..."
 echo
 
-sudo sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=sudo' /etc/sudoers;
-
 # Set up password-less sudo for the ubuntu user
 echo 'ubuntu ALL=(ALL) NOPASSWD:ALL' | sudo tee /etc/sudoers.d/99_user;
 sudo chmod 440 /etc/sudoers.d/99_user;
--- cloud-init-config/gateway-templates/11-sudoers.sh
+++ cloud-init-config/gateway-templates/11-sudoers.sh
@@ -3,8 +3,6 @@
 echo "Running script sudoers.sh..."
 echo
 
-sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=sudo' /etc/sudoers;
-
 # Set up password-less sudo for the ubuntu user
 echo 'ubuntu ALL=(ALL) NOPASSWD:ALL' > /etc/sudoers.d/99_user;
 chmod 440 /etc/sudoers.d/99_user;
