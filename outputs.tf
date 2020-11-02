19df983401ef870de699d49d786060b926076be0
healideal <mail@jens-fielenbach.de>
2020-11-02 12:51:50 +0100

    Output URL

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
-output "compartment" {
-  value = "${local.tenancy_name}/${oci_identity_compartment.one_per_subdomain.name}"
-}
-
-output "ubuntu-20-04-minimal-latest" {
+output "used_base_image" {
   value = data.oci_core_images.ubuntu-20-04-minimal.images.0.display_name
 }
 
-output "open_in_browser" {
+output "gateway_in_browser" {
   value = module.gateway.access_url
+}
+
+output "ssh_into_desktop" {
+  value = "ssh -i ${abspath(path.root)}/.ssh/privkey -o StrictHostKeyChecking=no ubuntu@${module.desktop_1.public_ip}"
 }
