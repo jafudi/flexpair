73a27a714ddae04d7211f738b3743d2fc0e18018
healideal <mail@jens-fielenbach.de>
2020-11-02 11:46:33 +0100

    Avoid installing and then uninstalling recommended packages

--- modules/desktop/ssh-remote-exec/lubuntu-desktop.sh
+++ modules/desktop/ssh-remote-exec/lubuntu-desktop.sh
@@ -10,40 +10,7 @@ sudo apt-get -qq update --fix-missing
 
 sudo -E apt-get -qq install \
 lubuntu-desktop \
-gdm3-
-
-sudo -E apt-get -qq install --no-install-recommends  \
-lightdm \
-lightdm-gtk-greeter \
-lightdm-gtk-greeter-settings \
-accountsservice \
-policykit-1 policykit-desktop-privileges
-
-sudo mkdir -p /etc/lightdm
-cat << EOF | sudo tee /etc/lightdm/lightdm.conf
-[SeatDefaults]
-user-session=lxqt
-greeter-session=lightdm-gtk-greeter
-EOF
-sudo systemctl enable lightdm.service
-sudo usermod -aG nopasswdlogin ubuntu
-
-sudo apt-get -qq purge \
-anacron- \
-bluedevil- bluez- bluez-cups- pulseaudio-module-bluetooth- \
-genisoimage- \
-lxqt-powermanagement- \
-mobile-broadband-provider-info- \
-modemmanager- \
-openprinting-ppds- \
-printer.*- \
-rfkill- \
-sddm.*- \
-ubuntu-release-upgrader-qt- \
-usb.*- \
-whoopsie- apport- \
-wireless-tools- wpasupplicant- \
-xscreensaver.*- \
+gdm3- \
 avahi-daemon- \
 firefox- \
 fwupd- fwupd-signed- \
@@ -65,5 +32,39 @@ acpi-support- \
 screengrab- \
 qps- \
 qlipper-
+
+sudo -E apt-get -qq install --no-install-recommends  \
+lightdm \
+lightdm-gtk-greeter \
+lightdm-gtk-greeter-settings \
+accountsservice \
+policykit-1 policykit-desktop-privileges
+
+sudo mkdir -p /etc/lightdm
+cat << EOF | sudo tee /etc/lightdm/lightdm.conf
+[SeatDefaults]
+user-session=lxqt
+greeter-session=lightdm-gtk-greeter
+EOF
+sudo systemctl enable lightdm.service
+sudo usermod -aG nopasswdlogin ubuntu
+
+# lubuntu-desktop depends on (for installation)
+sudo apt-get -qq purge \
+anacron- \
+bluedevil- bluez- bluez-cups- pulseaudio-module-bluetooth- \
+genisoimage- \
+lxqt-powermanagement- \
+mobile-broadband-provider-info- \
+modemmanager- \
+openprinting-ppds- \
+printer.*- \
+rfkill- \
+sddm.*- \
+ubuntu-release-upgrader-qt- \
+usb.*- \
+whoopsie- apport- \
+wireless-tools- wpasupplicant- \
+xscreensaver.*-
 
 sudo apt-get -qq autoremove
