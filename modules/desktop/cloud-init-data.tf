02a8e689441338a567bf4135310e890de105f4c5
healideal <mail@jens-fielenbach.de>
2020-11-10 17:49:48 +0100

    Add 10-cloud-config.yaml

--- modules/desktop/cloud-init-data.tf
+++ modules/desktop/cloud-init-data.tf
@@ -19,16 +19,16 @@ data "template_cloudinit_config" "desktop_config" {
     content_type = "text/cloud-boothook"
     content = file("${path.module}/init-scripts/03-sshd-config.sh")
   }
-//  part {
-//    content_type = "text/cloud-config"
-//    content = templatefile("${path.module}/init-scripts/10-cloud-config.yaml", {
-//      SSL_DOMAIN       = var.url.full_hostname
-//      DESKTOP_TIMEZONE = var.location_info.timezone_name
-//      DESKTOP_LOCALE   = var.location_info.locale_settings
-//      DESKTOP_USERNAME = var.desktop_username
-//      GATEWAY_USERNAME = var.gateway_username
-//    })
-//  }
+  part {
+    content_type = "text/cloud-config"
+    content = templatefile("${path.module}/init-scripts/10-cloud-config.yaml", {
+      SSL_DOMAIN       = var.url.full_hostname
+      DESKTOP_TIMEZONE = var.location_info.timezone_name
+      DESKTOP_LOCALE   = var.location_info.locale_settings
+      DESKTOP_USERNAME = var.desktop_username
+      GATEWAY_USERNAME = var.gateway_username
+    })
+  }
 //  part {
 //    content_type = "text/x-shellscript"
 //    content      = templatefile("${path.module}/init-scripts/11-sudoers.sh", {
