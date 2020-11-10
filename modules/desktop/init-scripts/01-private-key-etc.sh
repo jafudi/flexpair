80ddbea03d634278bc48b4a26f2f2c9408c33e70
healideal <mail@jens-fielenbach.de>
2020-11-10 03:18:57 +0100

    Start easy

--- modules/desktop/init-scripts/01-private-key-etc.sh
+++ modules/desktop/init-scripts/01-private-key-etc.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-#mkdir -p "/home/${DESKTOP_USERNAME}/.ssh"
-#
-#cat << EOF > "/home/${DESKTOP_USERNAME}/.ssh/vm_key"
-#${VM_PRIVATE_KEY}
-#EOF
-#chmod 600 "/home/${DESKTOP_USERNAME}/.ssh/vm_key"
+mkdir -p "/home/${DESKTOP_USERNAME}/.ssh"
+
+cat << EOF > "/home/${DESKTOP_USERNAME}/.ssh/vm_key"
+${VM_PRIVATE_KEY}
+EOF
+chmod 600 "/home/${DESKTOP_USERNAME}/.ssh/vm_key"
 
 #cat << EOF >> "/home/${DESKTOP_USERNAME}/.ssh/authorized_keys"
 #${VM_PUBLIC_KEY}
