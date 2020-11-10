182a3cce359c4f64b904516119fcd4b9f29a1e91
healideal <mail@jens-fielenbach.de>
2020-11-10 01:39:31 +0100

    Start easy

--- modules/desktop/desktop-instance.tf
+++ modules/desktop/desktop-instance.tf
@@ -38,14 +38,14 @@ resource "oci_core_instance" "desktop" {
     private_key = var.vm_mutual_keypair.private_key_pem
   }
 
-//  provisioner "remote-exec" {
-//    inline = [
-//      "echo Block until cloud-init finished...",
-//      "set +e",
-//      "cloud-init status --long --wait",
-//      "set -e"
-//    ]
-//  }
+  provisioner "remote-exec" {
+    inline = [
+      "echo Block until cloud-init finished...",
+      "set +e",
+      "cloud-init status --long --wait",
+      "set -e"
+    ]
+  }
 //
 //  # Must-haves
 //  provisioner "remote-exec" {
