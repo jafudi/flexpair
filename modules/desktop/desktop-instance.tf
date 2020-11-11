7377425b7561cb228dfbb186a913998ad3e20204
healideal <mail@jens-fielenbach.de>
2020-11-12 00:33:54 +0100

    Fail fast

--- modules/desktop/desktop-instance.tf
+++ modules/desktop/desktop-instance.tf
@@ -36,6 +36,7 @@ resource "oci_core_instance" "desktop" {
     port        = 22
     user        = var.desktop_username
     private_key = var.vm_mutual_keypair.private_key_pem
+    timeout = "1m"
   }
 
   provisioner "remote-exec" {
