7e6ce43bb9682f115baf32d36e085158896c867e
healideal <mail@jens-fielenbach.de>
2020-11-12 01:48:42 +0100

    ${DESKTOP_USERNAME}

--- modules/desktop/init-scripts/01-private-key-etc.sh
+++ modules/desktop/init-scripts/01-private-key-etc.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-mkdir -p /home/ubuntu/uploads
-mkdir -p /home/ubuntu/Desktop/Uploads
-chown -R ubuntu /home/ubuntu
+mkdir -p /home/${DESKTOP_USERNAME}/uploads
+mkdir -p /home/${DESKTOP_USERNAME}/Desktop/Uploads
+chown -R ${DESKTOP_USERNAME} /home/${DESKTOP_USERNAME}
 
-cat << EOF > /home/ubuntu/.ssh/vm_key
+cat << EOF > /home/${DESKTOP_USERNAME}/.ssh/vm_key
 ${VM_PRIVATE_KEY}
 EOF
-chmod 600 /home/ubuntu/.ssh/vm_key
+chmod 600 /home/${DESKTOP_USERNAME}/.ssh/vm_key
