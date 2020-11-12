8b6fd612d257bd269ca45401e7acee209a89706e
healideal <mail@jens-fielenbach.de>
2020-11-12 01:36:31 +0100

    Roll back to master

--- modules/desktop/init-scripts/01-private-key-etc.sh
+++ modules/desktop/init-scripts/01-private-key-etc.sh
@@ -1,16 +1,10 @@
 #!/usr/bin/env bash
 
-mkdir -p "/home/${DESKTOP_USERNAME}/.ssh"
+mkdir -p /home/ubuntu/uploads
+mkdir -p /home/ubuntu/Desktop/Uploads
+chown -R ubuntu /home/ubuntu
 
-cat << EOF > "/home/${DESKTOP_USERNAME}/.ssh/vm_key"
+cat << EOF > /home/ubuntu/.ssh/vm_key
 ${VM_PRIVATE_KEY}
 EOF
-chmod 600 "/home/${DESKTOP_USERNAME}/.ssh/vm_key"
-
-cat << EOF > "/home/${DESKTOP_USERNAME}/.ssh/authorized_keys"
-${VM_PUBLIC_KEY}
-EOF
-chmod 600 "/home/${DESKTOP_USERNAME}/.ssh/authorized_keys"
-
-mkdir -p "/home/${DESKTOP_USERNAME}/Desktop/Uploads"
-chown -R "${DESKTOP_USERNAME}" "/home/${DESKTOP_USERNAME}"
+chmod 600 /home/ubuntu/.ssh/vm_key
