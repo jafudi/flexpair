784fc54c5effcfbe1ecbdb7b126f09b2fda0e9c6
healideal <mail@jens-fielenbach.de>
2020-11-13 02:38:31 +0100

    Let cloud-init configure passwordless sudo for ubuntu user

--- modules/desktop/init-scripts/11-sudoers.sh
+++ modules/desktop/init-scripts/11-sudoers.sh
@@ -1,7 +1,3 @@
 #!/bin/sh -eux
 
-# Set up password-less sudo
-echo "${DESKTOP_USERNAME} ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/99_user;
-chmod 440 /etc/sudoers.d/99_user;
-
 chown -R "${DESKTOP_USERNAME}" "/home/${DESKTOP_USERNAME}"
--- modules/gateway/init-scripts/11-sudoers.sh
+++ modules/gateway/init-scripts/11-sudoers.sh
@@ -1,10 +1,3 @@
 #!/bin/sh -eux
 
-echo "Running script sudoers.sh..."
-echo
-
-# Set up password-less sudo
-echo "${GATEWAY_USERNAME} ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/99_user;
-chmod 440 /etc/sudoers.d/99_user;
-
 chown -R "${GATEWAY_USERNAME}" "/home/${GATEWAY_USERNAME}"
