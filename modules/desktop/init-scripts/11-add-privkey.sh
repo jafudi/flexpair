4865f821fc48bb50c65ee7ec75f5c1a70658224f
healideal <mail@jens-fielenbach.de>
2020-11-13 16:08:59 +0100

    Fix permissions issue

--- modules/desktop/init-scripts/11-add-privkey.sh
+++ modules/desktop/init-scripts/11-add-privkey.sh
@@ -3,5 +3,5 @@
 cat << EOF > /home/${DESKTOP_USERNAME}/.ssh/vm_key
 ${VM_PRIVATE_KEY}
 EOF
-chown "${DESKTOP_USERNAME}" "/home/${DESKTOP_USERNAME}/.ssh/vm_key"
+chown -R "${DESKTOP_USERNAME}" "/home/${DESKTOP_USERNAME}"
 chmod 600 "/home/${DESKTOP_USERNAME}/.ssh/vm_key"
--- modules/gateway/init-scripts/11-add-privkey.sh
+++ modules/gateway/init-scripts/11-add-privkey.sh
@@ -3,7 +3,7 @@
 cat << EOF > "/home/${GATEWAY_USERNAME}/.ssh/vm_key"
 ${VM_PRIVATE_KEY}
 EOF
-chown "${GATEWAY_USERNAME}" "/home/${GATEWAY_USERNAME}/.ssh/vm_key"
+chown -R "${GATEWAY_USERNAME}" "/home/${GATEWAY_USERNAME}"
 chmod 600 "/home/${GATEWAY_USERNAME}/.ssh/vm_key"
 
 
