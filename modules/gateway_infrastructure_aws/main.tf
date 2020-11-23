85706b18d6a363f1ca1a852ffcc76f9088b44ac4
healideal <mail@jens-fielenbach.de>
2020-11-23 02:52:45 +0100

    Attempt to connect via SSH must fail for two reasons: 1. No port 22 open 2. No public SSH key placed on server

--- modules/gateway_infrastructure_aws/main.tf
+++ modules/gateway_infrastructure_aws/main.tf
@@ -15,4 +15,27 @@ resource "aws_instance" "gateway" {
   monitoring = false
   subnet_id = var.network_config.subnet_id
   vpc_security_group_ids = var.network_config.vpc_security_group_ids
+
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    port        = 22
+    user        = var.gateway_username
+    private_key = var.vm_mutual_keypair.private_key_pem
+    timeout = "30s"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "cat /var/log/cloud-init-output.log",
+      "tail -f /var/log/cloud-init-output.log | sed '/^.*finished at.*$/ q'"
+    ]
+    on_failure = continue
+  }
+
+  provisioner "file" {
+    source      = "${path.root}/uploads/"
+    destination = "/home/${var.gateway_username}/uploads"
+    on_failure  = continue
+  }
 }
