50f4ab1a75231e8ba01a7d8d5d64f15a562bd59a
healideal <mail@jens-fielenbach.de>
2020-12-05 23:34:27 +0100

    Swap desktop and gateway roles between AWS and OCI

--- main.tf
+++ main.tf
@@ -25,8 +25,8 @@ module "amazon_infrastructure" {
 module "credentials_generator" {
   registered_domain     = var.registered_domain
   subdomain_proposition = "${var.TFC_CONFIGURATION_VERSION_GIT_BRANCH}-branch-${var.TFC_WORKSPACE_NAME}"
-  gateway_username      = module.amazon_infrastructure.cloud_account_name
-  desktop_username      = module.oracle_infrastructure.cloud_account_name
+  gateway_username      = module.oracle_infrastructure.cloud_account_name
+  desktop_username      = module.amazon_infrastructure.cloud_account_name
   source                = "./modules/credentials_generator"
   // below variables are specific to dynv6.com DNS as an RFC2136 implementation
   rfc2136_name_server    = var.rfc2136_name_server
