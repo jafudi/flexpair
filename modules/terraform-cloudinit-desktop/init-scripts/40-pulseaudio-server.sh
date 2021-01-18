a833da53b12afe36ee8cfa9d09c6692a78b29124
healideal <mail@jens-fielenbach.de>
2021-01-18 13:17:12 +0100

    Set PulseAudio default sample rate to Mumble's 48 kHz and use a high quality resampling method (soxr-hq) if playbacks have different inherent sample rate.

--- modules/terraform-cloudinit-desktop/init-scripts/40-pulseaudio-server.sh
+++ modules/terraform-cloudinit-desktop/init-scripts/40-pulseaudio-server.sh
@@ -23,3 +23,29 @@ set-default-source DesktopAudio.monitor
 load-module module-native-protocol-tcp auth-anonymous=1
 load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1
 EOF
+
+# http://manpages.ubuntu.com/manpages/focal/man5/pulse-daemon.conf.5.html
+cat << EOF > "/home/${DESKTOP_USERNAME}/.config/pulse/daemon.conf"
+high-priority = yes
+; nice-level = -11
+
+realtime-scheduling = yes
+; realtime-priority = 5
+
+resample-method = soxr-hq
+; avoid-resampling = false
+; enable-remixing = yes
+; remixing-use-all-sink-channels = yes
+; remixing-produce-lfe = no
+; remixing-consume-lfe = no
+
+; default-sample-format = s16le
+default-sample-rate = 48000
+alternate-sample-rate = 48000
+; default-sample-channels = 2
+; default-channel-map = front-left,front-right
+
+; enable-deferred-volume = yes
+deferred-volume-safety-margin-usec = 1
+; deferred-volume-extra-delay-usec = 0
+EOF
