bad506bcfb9ac2638443f9d63a7fab68b0de052f
healideal <mail@jens-fielenbach.de>
2021-01-27 17:52:36 +0100

    Rely on pre-commit for linting

---
+++ run-pre-commit.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+brew install pre-commit gawk terraform-docs tflint coreutils
+
+DIR=~/.git-template
+git config --global init.templateDir ${DIR}
+pre-commit init-templatedir -t pre-commit ${DIR}
+
+pre-commit run -a
