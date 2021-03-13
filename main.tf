0216165cf435844bb78516c22376610d3575fd45
healideal <mail@jens-fielenbach.de>
2021-03-13 09:06:23 +0100

    Roll back to gateway version 1.2.2 and see if that still works

--- main.tf
+++ main.tf
@@ -49,7 +49,7 @@ module "gateway_installer" {
   email_config           = module.credentials_generator.email_config
   docker_compose_release = local.docker_compose_release
   source                 = "app.terraform.io/jafudi/station/cloudinit"
-  version                = "1.2.3"
+  version                = "1.2.2"
 }
 
 locals {
