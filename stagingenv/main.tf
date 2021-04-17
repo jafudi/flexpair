c2677c5f6131b7f79157f62c7f76c208829a5fb9
healideal <mail@jens-fielenbach.de>
2021-04-17 22:42:44 +0200

    Make credentials sensitive

--- stagingenv/main.tf
+++ stagingenv/main.tf
@@ -1,3 +1,6 @@
+resource "random_pet" "subdomain" {
+}
+
 locals {
   deployment_tags = {
     terraform_run_id = var.TFC_RUN_ID
@@ -7,7 +10,7 @@ locals {
   org_list        = split("/", var.TFC_WORKSPACE_SLUG)
   organization    = local.org_list[0]
   workspace       = local.org_list[1]
-  valid_subdomain = lower(replace(local.workspace, "/[_\\W]/", "-"))
+  valid_subdomain = random_pet.subdomain # in PROD: lower(replace(local.workspace, "/[_\\W]/", "-"))
   full_hostname   = "${local.valid_subdomain}.${var.registered_domain}"
   admin_name      = "${local.organization}admin"
 }
