9dcf789b2c034d8b437fdf6defd9fa1222b4ca04
healideal <mail@jens-fielenbach.de>
2021-04-17 22:43:36 +0200

    Make credentials sensitive

--- prod-hotfix/main.tf
+++ prod-hotfix/main.tf
@@ -1,16 +1,17 @@
-locals {
-  org_list        = split("/", var.TFC_WORKSPACE_SLUG)
-  organization    = local.org_list[0]
-  workspace       = local.org_list[1]
-  valid_subdomain = lower(replace(local.workspace, "/[_\\W]/", "-"))
-  full_hostname   = "${local.valid_subdomain}.${var.registered_domain}"
-  admin_name      = "${local.organization}admin"
+data "terraform_remote_state" "prod" {
+  backend = "remote"
+  config = {
+    organization = "jafudi"
+    workspaces = {
+      name = "apr15"
+    }
+  }
 }
 
 provider "guacamole" {
-  url                      = "https://${local.full_hostname}/guacamole"
-  username                 = local.admin_name
-  password                 = "guacadmin"
+  url                      = data.terraform_remote_state.prod.outputs.guacamole_endpoint
+  username                 = data.terraform_remote_state.prod.outputs.guacamole_admin_username
+  password                 = data.terraform_remote_state.prod.outputs.guacamole_admin_password
   disable_tls_verification = true
   disable_cookies          = true
 }
