3541ff2e598c2407a77cc8d66fc4118d8eec68ba
healideal <mail@jens-fielenbach.de>
2021-04-19 14:52:33 +0200

    Bugfix

--- prod-hotfix/main.tf
+++ prod-hotfix/main.tf
@@ -13,19 +13,19 @@ resource "guacamole_connection_vnc" "collaborate" {
     failover_only = false
   }
   parameters {
-    hostname            = local.first_vnc_connection.hostname
-    port                = local.first_vnc_connection.vnc_port
-    username            = local.first_vnc_connection.username
-    password            = local.first_vnc_connection.password
-    sftp_enable         = true
-    sftp_hostname       = "gateway"
-    sftp_username       = local.gateway_username
-    sftp_directory      = "/home/${local.gateway_username}/uploads"
-    sftp_root_directory = "/home/${local.gateway_username}/uploads"
-    readonly            = false
-    enable_audio        = false
-    disable_copy        = false
-    disable_paste       = false
+    hostname              = local.first_vnc_connection.hostname
+    port                  = local.first_vnc_connection.vnc_port
+    username              = local.first_vnc_connection.username
+    password              = local.first_vnc_connection.password
+    sftp_enable           = true
+    sftp_hostname         = "gateway"
+    sftp_username         = local.gateway_username
+    sftp_upload_directory = "/home/${local.gateway_username}/uploads"
+    sftp_root_directory   = "/home/${local.gateway_username}/uploads"
+    readonly              = false
+    enable_audio          = false
+    disable_copy          = false
+    disable_paste         = false
   }
 }
 
@@ -36,19 +36,19 @@ resource "guacamole_connection_vnc" "view_only" {
     failover_only = false
   }
   parameters {
-    hostname            = local.first_vnc_connection.hostname
-    port                = local.first_vnc_connection.vnc_port
-    username            = local.first_vnc_connection.username
-    password            = local.first_vnc_connection.password
-    sftp_enable         = false
-    sftp_hostname       = "gateway"
-    sftp_username       = local.gateway_username
-    sftp_directory      = "/home/${local.gateway_username}/uploads"
-    sftp_root_directory = "/home/${local.gateway_username}/uploads"
-    readonly            = true
-    enable_audio        = false
-    disable_copy        = true
-    disable_paste       = true
+    hostname              = local.first_vnc_connection.hostname
+    port                  = local.first_vnc_connection.vnc_port
+    username              = local.first_vnc_connection.username
+    password              = local.first_vnc_connection.password
+    sftp_enable           = false
+    sftp_hostname         = "gateway"
+    sftp_username         = local.gateway_username
+    sftp_upload_directory = "/home/${local.gateway_username}/uploads"
+    sftp_root_directory   = "/home/${local.gateway_username}/uploads"
+    readonly              = true
+    enable_audio          = false
+    disable_copy          = true
+    disable_paste         = true
   }
 }
 
@@ -64,7 +64,7 @@ resource "guacamole_connection_ssh" "admin_console" {
     username            = local.gateway_username
     sftp_root_directory = "/home/${local.gateway_username}/uploads"
     readonly            = false
-    enable_sftp         = true
+    sftp_enable         = true
     color_scheme        = "green-black"
   }
 }
