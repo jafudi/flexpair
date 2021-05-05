b37548742ebf6fa7f51361b00e060dbc87b333bc
healideal <mail@jens-fielenbach.de>
2021-05-05 22:40:19 +0200

    install genisoimage

--- stagingenv/main.tf
+++ stagingenv/main.tf
@@ -109,7 +109,7 @@ resource "null_resource" "desktop_config_iso" {
   provisioner "local-exec" {
     working_dir = "${path.root}/../uploads/desktop-config"
     interpreter = ["/bin/bash", "-c"]
-    command     = "genisoimage  -output config.iso -volid cidata -joliet -rock user-data meta-data"
+    command     = "apt-get update; apt-get -qq install genisoimage; genisoimage  -output config.iso -volid cidata -joliet -rock user-data meta-data"
   }
 }
 
