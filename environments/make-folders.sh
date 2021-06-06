7dd3040a07a6e008d526ceae5f0920308704a6ab
healideal <mail@jens-fielenbach.de>
2021-06-06 12:14:14 +0200

    make-folders.sh

---
+++ environments/make-folders.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+supported_clouds="aws oracle azure"
+
+for gateway in ${supported_clouds}
+do
+    for desktop in ${supported_clouds} hybrid
+    do
+        if [ "$desktop" != "$gateway" ]
+        then
+            folder_name="$gateway-$desktop"
+        else
+            folder_name="$gateway"
+        fi
+        mkdir -p "${folder_name}"
+    done
+done
