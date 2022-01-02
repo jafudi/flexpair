2b843c9d8aae4f3c1928cb37f9a1c487ffa16f56
Jens Fielenbach <flexpair@icloud.com>
2022-01-03 00:45:45 +0100

    CNAME + A record + server internal redirect

--- environments/aws/provider-dns.tf
+++ environments/aws/provider-dns.tf
@@ -29,20 +29,24 @@ variable "registered_domain" {
   }
 }
 
+locals {
+  pw = module.credentials_generator.murmur_credentials.password
+}
+
 
 resource "dnsimple_zone_record" "redirect_to_demo" {
   zone_name = var.registered_domain
   name      = "demo"
-  value     = "login.${local.valid_subdomain}.${var.registered_domain}"
+  value     = "guest${local.pw}.${local.valid_subdomain}.${var.registered_domain}"
   type      = "CNAME"
   ttl       = 60
 }
 
-resource "dnsimple_zone_record" "add_credentials" {
+resource "dnsimple_zone_record" "demo_guest_login" {
   zone_name = var.registered_domain
-  name      = "login.${local.valid_subdomain}"
-  value     = "${module.credentials_generator.browser_url}&username=guest"
-  type      = "URL"
+  name      = "guest${local.pw}.${local.valid_subdomain}"
+  value     = module.gateway_machine.public_ip
+  type      = "A"
   ttl       = 60
 }
 
