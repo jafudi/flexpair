2c67ea1ed1840f22f247663c7fb36474fcc8a042
Flexpair <github.com@flexpair.com>
2024-08-30 02:40:10 +0200

    Upgrade provider

--- ws-user-access/versions.tf
+++ ws-user-access/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     guacamole = {
       source  = "techBeck03/guacamole"
-      version = "1.2.7" // parameterize together with Guacamole version
+      version = "1.2.10" // parameterize together with Guacamole version
     }
   }
   required_version = ">= 0.12.26"
